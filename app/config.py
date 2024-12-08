ed1a53e4ee7702fdbe85fb15e8373af391652986
Олег Лозовий <davemag9@MacBook-Air-Oleg.local>
2024-12-08 14:16:45 +0200

    commit 2

--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
-PATH_PREPROCESSED_DOCS = '../data/preprocessed_text.json'
-PATH_METADATA = '../data/metadata.json'
-PATH_BASE = '../data/faiss_base.faiss'
+
+DATA_FOLDER_ABSOLUTE_PATH = '/Users/davemag9/Desktop/RAG_application/app/data'
+
+PATH_PREPROCESSED_DOCS = f'{DATA_FOLDER_ABSOLUTE_PATH}/preprocessed_text.json'
+PATH_METADATA = f'{DATA_FOLDER_ABSOLUTE_PATH}/metadata.json'
+PATH_BASE = f'{DATA_FOLDER_ABSOLUTE_PATH}/faiss_base.faiss'
