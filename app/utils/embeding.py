e0d4bd36fe54782ac8016bac4ec802d35006f94f
Олег Лозовий <davemag9@MacBook-Air-Oleg.local>
2024-12-09 14:20:37 +0200

    commit 5

--- app/utils/embeding.py
+++ app/utils/embeding.py
@@ -1,13 +1,12 @@
 from sentence_transformers import SentenceTransformer
 
+
 class Embedding:
     def __init__(self, model_name: str = 'all-MiniLM-L6-v2'):
         self.model = SentenceTransformer(model_name, trust_remote_code=True, revision="main")
 
-    def query_embedding(self, query):
-        query_embed = self.model.encode([query], normalize_embeddings=True)
-        return query_embed
+    def query_embedding(self, query: str):
+        return self.model.encode([query], normalize_embeddings=True)
 
-    def get_embedding(self, texts):
-        embeddings = self.model.encode(texts, normalize_embeddings=True)
-        return embeddings
+    def get_embedding(self, texts: list[str]):
+        return self.model.encode(texts, normalize_embeddings=True)
